c8609a8d9d9d4503174275d569fd27aa19788ff8
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-12-02 08:37:57 +0300

    working with data structures

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,50 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    food_name = [food['name'] for food in spicy_foods]
+    return food_name
+
+get_names(spicy_foods)
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    food = [heat_food for heat_food in spicy_foods if heat_food['heat_level'] > 5]
+    return food
+get_spiciest_foods(spicy_foods)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for spici_food in spicy_foods:
+       heat_level = '🌶' * spici_food['heat_level']
+
+       new_outlook = f"{spici_food['name']} ({spici_food['cuisine']}) | Heat Level: {heat_level}"
+       print(new_outlook)
+print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    # type_cuisine = [by_cuisine for by_cuisine in spicy_foods if cuisine == by_cuisine['cuisine']]
+    for by_cuisine in spicy_foods:
+        if cuisine == by_cuisine['cuisine']:
+            type_cuisine = by_cuisine
+    return type_cuisine
+get_spicy_food_by_cuisine(spicy_foods, 'Sichuan')
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for spici_food in spicy_foods:
+        heat_level = '🌶' * spici_food['heat_level']
+
+        if spici_food['heat_level'] > 5:
+            new_outlook = f"{spici_food['name']} ({spici_food['cuisine']}) | Heat Level: {heat_level}"
+            print(new_outlook)
+           
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+        total_level = sum(spici_food['heat_level'] for spici_food in spicy_foods)
+        av_heatlevel = int(total_level / len(spicy_foods))
+        print(av_heatlevel)
+        return av_heatlevel
+get_average_heat_level(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+create_spicy_food(spicy_foods, spicy_foods)
